82826f3e3896e18f080ee781bc35953bb29fd93c
William Yun <william-yun@outlook.com>
2019-11-26 09:49:49 -0300

    Product

--- src/main/java/com/williamyun/course/entities/Product.java
+++ src/main/java/com/williamyun/course/entities/Product.java
@@ -12,27 +12,34 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double prince;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double prince, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.prince = prince;
+		this.imgUrl = imgUrl;
 	}
+
 
 	public Long getId() {
 		return id;
@@ -49,10 +56,35 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDescription() {
+		return description;
 	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrince() {
+		return prince;
+	}
+
+	public void setPrince(Double prince) {
+		this.prince = prince;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -70,7 +102,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -78,8 +110,6 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-
-
-
-
+	
+	
 }
